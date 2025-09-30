db35ce80b39b4f71261542bb88574985873f896c
Pthetinh <phanthetinh2004@gmail.com>
2025-10-01 03:47:56 +0700

    feat: implement basic Vector class template with constructors and destructor

---
+++ Lab1/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+namespace myVector {
+    template<class T>
+    class Vector {
+    private:
+        T* data;
+        size_t size;
+
+    public:
+        static constexpr double EPS = 1e-8;
+
+        Vector() : data(nullptr), size(0) {}
+        Vector(size_t n, const T* init) : data(new T[n]), size(n) {
+            for (size_t i = 0; i < size; ++i)
+                data[i] = init[i];
+        }
+        Vector(size_t n, const T& fillValue = T()) : data(new T[n]), size(n) {
+            for (size_t i = 0; i < size; ++i)
+                data[i] = fillValue;
+        }
+        Vector(const Vector& other) : data(new T[other.size]), size(other.size) {
+            for (size_t i = 0; i < size; ++i) data[i] = other.data[i];
+        }
+        Vector& operator=(const Vector& other) {
+            if (this == &other) return *this;
+            delete[] data;
+            size = other.size;
+            data = new T[size];
+            for (size_t i = 0; i < size; ++i) data[i] = other.data[i];
+            return *this;
+        }
+
+        T& operator[](size_t index) {
+            if (index >= size) throw out_of_range("Invalid index!");
+            return data[index];
+        }
+        const T& operator[](size_t index) const {
+            if (index >= size) throw out_of_range("Invalid index!");
+            return data[index];
+        }
+
+        size_t getSize() const {
+            return size;
+        }
+
+        ~Vector() { delete[] data; }
+
+        template<typename U>
+        friend std::ostream& operator<<(std::ostream& os, const Vector<U>& v);
+    };
+
+    template<typename T>
+    std::ostream& operator<<(std::ostream& os, const Vector<T>& v) {
+        os << "{ ";
+        for (size_t i = 0; i < v.size; ++i) os << v[i] << ' ';
+        os << "}\n";
+        return os;
+    }
+}
+
+int main() {
+    double arr[] = { 1.0, 2.0, 3.0 };
+    myVector::Vector<double> v(3, arr);
+    cout << "Vector: " << v;
+    cout << "v[0] = " << v[0] << ", v[1] = " << v[1] << ", v[2] = " << v[2] << endl;
+    return 0;
+}
